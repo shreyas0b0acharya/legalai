b25c028dc454f2a2d73923467b017a136a79dc64
Shreyas B.Acharya <168179668+shreyas0b0acharya@users.noreply.github.com>
2025-12-01 03:22:55 +0530

    Update firebase.js

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6KDFmSc2Vh9C1yv-zdgWLNHtcOJOMY2Y",
-  authDomain: "legalai-496b8.firebaseapp.com",
-  projectId: "legalai-496b8",
-  storageBucket: "legalai-496b8.firebasestorage.app",
-  messagingSenderId: "899769528644",
-  appId: "1:899769528644:web:7873cfa6e18b253eeda711",
-  measurementId: "G-S8J45XGM24"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
